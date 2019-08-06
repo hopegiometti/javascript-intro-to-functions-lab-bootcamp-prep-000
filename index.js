e9ec9e42f8bfc1270f0947606588a0d6c8269a42
Hope Giometti <hopegiometti@gmail.com>
2019-08-06 21:18:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string === lowercase
-  return "I can\'t hear you!"
-  else if string === uppercase
-  return "YES INDEED"
-  else string === "I love you, Grandma."
-  return "I love you, too."
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
+  }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED"
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
